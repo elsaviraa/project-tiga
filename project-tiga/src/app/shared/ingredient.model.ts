6e9afe932890b0d2ec53a013c7e259ae8a56ca49
elsaviraa <37010727+elsaviraa@users.noreply.github.com>
2018-11-02 11:53:52 +0700

    Add files via upload

---
+++ project-tiga/src/app/shared/ingredient.model.ts
@@ -0,0 +1,17 @@
+export class Ingredient{
+
+    // 1. Cara pertama
+    // public name:string;
+    // public amount:number;
+
+    // constructor(name:string,amount:number){
+    //     this.name=name;
+    //     this.amount=amount;
+    // }
+
+    // 2.Cara Kedua
+
+    constructor(public name:string, public amount:number){
+        
+    }
+}
